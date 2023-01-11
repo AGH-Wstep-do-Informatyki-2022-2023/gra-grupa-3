a283a200ce353afa575630da3a394ea56652202c
wiktorDeka <wiktordeka@student.agh.edu.pl>
2023-01-11 17:44:13 +0100

    feature(game-manager): dodanie game manager i grafik

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import button
 
 class Game:
     screen = None
@@ -29,7 +29,7 @@ class Game:
 
             pressed = pygame.key.get_pressed()
             if pressed[pygame.K_LEFT]:
-                hero.x -= 2 if hero.x > 20 else 0 
+                hero.x -= 2 if hero.x > 20 else 0
             elif pressed[pygame.K_RIGHT]:
                 hero.x += 2 if hero.x < width - 20 else 0
 
@@ -59,14 +59,14 @@ class Game:
 
                 if (alien.x) > (self.width - (self.width/20)):
                     for tabalien in self.aliens:
-                        if tabalien.row == alien.row: 
+                        if tabalien.row == alien.row:
                             tabalien.direction = 0
                             alien.direction = 0
 
                 elif (alien.x) < (self.width/30):
                     alien.x += (alien.sidewalkStep *2)
                     for tabalien in self.aliens:
-                        if tabalien.row == alien.row: 
+                        if tabalien.row == alien.row:
                             tabalien.direction = 1
                             alien.direction = 1
 
@@ -74,7 +74,7 @@ class Game:
                 #if self.now - self.alienAttackLast >= self.alienAttackCooldown:
                 #    self.alienAttackLast = self.now
                 #    self.alienBullets.append(AlienBullet(self, alien.x, alien.y))
-                
+
 
             for rocket in self.rockets:
                 rocket.draw()
@@ -113,7 +113,7 @@ class Alien:
         if now - self.goingDownLast >= self.goingDownCooldown:
             self.goingDownLast = now
             self.y += 30
-            
+
 
     def sidewalk(self):
         now = pygame.time.get_ticks()
@@ -127,7 +127,7 @@ class Alien:
                 left()
             else:
                 right()
-           
+
 
     def draw(self):
         pygame.draw.rect(self.game.screen,
@@ -191,7 +191,7 @@ class Generator:
         start=margin + int(game.height / 12)
         stop=start + int(game.height / 16)
         step=int(game.height / 20)
-        
+
         row = 0
 
         for y in range(start, stop, step): # 2 row
@@ -220,7 +220,7 @@ class Generator:
             for x in range(gameMarginStart, gameMarginStop, width): # 15 pos
                 pos += 1
                 game.aliens.append(AlienBlue(game, x, y,row,pos))
-        
+
 
 class Rocket:
     def __init__(self, game, x, y):
@@ -252,4 +252,79 @@ class Rocket:
 
 
 if __name__ == '__main__':
-    game = Game(1200, 800) # !! tu tworząc obiekt podajemy wymiary okna gry !!
+    game = Game(800, 800) # !! tu tworząc obiekt podajemy wymiary okna gry !!
+
+pygame.init()  # create game window
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Main Menu")
+
+game_paused = False
+menu_state = "main"
+font = pygame.font.SysFont("arialblack", 40)
+TEXT_COL = (255, 255, 255)
+bg_img = pygame.image.load("images/background.png").convert_alpha()
+resume_img = pygame.image.load("images/button_resume.png").convert_alpha()
+options_img = pygame.image.load("images/button_options.png").convert_alpha()
+quit_img = pygame.image.load("images/button_quit.png").convert_alpha()
+video_img = pygame.image.load('images/button_video.png').convert_alpha()
+audio_img = pygame.image.load('images/button_audio.png').convert_alpha()
+keys_img = pygame.image.load('images/button_keys.png').convert_alpha()
+back_img = pygame.image.load('images/button_back.png').convert_alpha()
+
+resume_button = button.Button(200, 400, resume_img, 1)
+options_button = button.Button(450, 550, options_img, 1)
+quit_button = button.Button(450, 650, quit_img, 1)
+video_button = button.Button(450, 550, video_img, 1)
+audio_button = button.Button(450, 550, audio_img, 1)
+keys_button = button.Button(246, 325, keys_img, 1)
+back_button = button.Button(332, 450, back_img, 1)
+
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+
+run = True
+while run:
+
+    screen.fill((52, 78, 91))
+
+    # check if game is paused
+    if game_paused:
+        # check menu state
+        if menu_state == "main":
+            # draw pause screen buttons
+            if resume_button.draw(screen):
+                game_paused = False
+            if options_button.draw(screen):
+                menu_state = "options"
+            if quit_button.draw(screen):
+                run = False
+        # check if the options menu is open
+        if menu_state == "options":
+            # draw the different options buttons
+            if video_button.draw(screen):
+                print("Video Settings")
+            if audio_button.draw(screen):
+                print("Audio Settings")
+            if keys_button.draw(screen):
+                print("Change Key Bindings")
+            if back_button.draw(screen):
+                menu_state = "main"
+    else:
+        draw_text("Naciśnij SPACE aby kontynuować", font, TEXT_COL, 40, 400)
+
+for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+            game_paused = True
+    if event.type == pygame.QUIT:
+        run = False
+
+pygame.display.update()
+
+pygame.quit()
